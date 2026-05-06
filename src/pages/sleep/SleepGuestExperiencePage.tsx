d1230d6f282933a5a0fdc8d662e58ec7e49b9b8f
Bigajax <rafaelr.barbon@gmail.com>
2026-05-06 15:55:21 -0300

    feat: add SleepMeditationsPage and SleepGuestExperiencePage wrappers

---
+++ src/pages/sleep/SleepGuestExperiencePage.tsx
@@ -0,0 +1,5 @@
+import { SleepMeditationExperience } from '@/components/sleep/SleepMeditationExperience';
+
+export default function SleepGuestExperiencePage() {
+  return <SleepMeditationExperience mode="guest" />;
+}
---
+++ src/pages/sleep/SleepMeditationsPage.tsx
@@ -0,0 +1,5 @@
+import { SleepMeditationExperience } from '@/components/sleep/SleepMeditationExperience';
+
+export default function SleepMeditationsPage() {
+  return <SleepMeditationExperience mode="app" />;
+}
